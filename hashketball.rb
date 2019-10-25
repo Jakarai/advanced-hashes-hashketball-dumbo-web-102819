896df101e554c3d0bfee52601df3b3001faf2155
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-25 06:19:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -148,7 +148,7 @@ def shoe_size(players_name)
     end
   end
 end
-def team_colors(players_name)
+def team_colors(team_name)
   game_hash.each do |place, team|
         if team[:team_name] == team_name
            return team[:colors]
