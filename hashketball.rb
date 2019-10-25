27c409c69ee9cf6590566802cd8040ef9559cc0e
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-25 04:43:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -126,8 +126,7 @@ def num_points_scored(players_name)
     team.each do |attributes, data|
       if attributes == :players
         data.each do |player|
-          if player[:player_name] == players_name
-             player[:points]
+
           end
         end
       end
