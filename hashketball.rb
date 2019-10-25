8ce6f94524ada8f2ea50643d50d9c53160aafe90
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-25 14:53:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -163,7 +163,7 @@ end
 
 def player_numbers(team_name)
   game_hash.each do |place, team|
-    if keys[:team_name] == team_name
+    if team[:team_name] == team_name
       return keys[:players].map { |player| player[:number] }
     end
   end
