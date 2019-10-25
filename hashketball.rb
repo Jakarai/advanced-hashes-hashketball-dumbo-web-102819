5aa99b2557ad6cb7a3c26c972e9f08e7c3882eb4
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-25 14:49:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,7 +168,7 @@ def player_numbers(team_name)
       team.each do |attributes, data|
         if attributes == :player 
           data.each do |player|
-            nums << player[]
+            nums << player[:number]
             
           end  
        end 
